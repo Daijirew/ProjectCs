0799d35d998982ce7ad1c4330ffe161d3cc0dc65
Poomrapee Patum <daijirew123@gmail.com>
2024-11-01 21:45:42 +0700

    Firebase Link and Sigup
    ลิ้งไฟเบสกับเข้าสู่ระบบนิดหน่อย(แต่ยังไม่ได้)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:myproject/pages.dart/buttomnav.dart';
 import 'package:myproject/pages.dart/home.dart';
 import 'package:myproject/pages.dart/login.dart';
 import 'package:myproject/pages.dart/onboard.dart';
 import 'package:myproject/pages.dart/sigup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Onboard(),
+      home: LogIn(),
     );
   }
 }
